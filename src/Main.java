00d5351f2bd1bfab8811935cf54ef362b8469b6c
GerbeRussia <adgar830@gmail.com>
2022-12-15 23:41:13 +0700

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Циклы Часть 2
+        // Задание 1
+        int totalSavings = 0;
+        int savings = 15_000;
+        int month = 0;
+        while (totalSavings <= 2_459_000){
+            month = month + 1;
+            totalSavings = savings * month;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + totalSavings + " рублей");
+        }
     }
 }
