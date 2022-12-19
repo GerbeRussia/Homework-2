9f287a96ba194394684797b2b64685d02dd76d3f
GerbeRussia <adgar830@gmail.com>
2022-12-19 21:08:33 +0700

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // Циклы Часть 2
+        // Задание 4
+        int Savings = 15_000;
+        int Month = 1;
+        for (;Savings < 12_000_000; Month ++) {
+            Savings = Savings + (Savings * 7 / 100);
+            System.out.println("Сумма накоплений в " + Month + " месяце, равняется " + Savings + " рублей");
+        }
     }
 }
